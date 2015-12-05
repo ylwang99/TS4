6e1b99bb4b207ac4affebd35e4364665bafe8670
ylwang99 <wg3999@gmail.com>
2015-12-04 22:41:53 -0500

    process edinburgh corpus

---
+++ ts4-core/src/main/java/ts4/ts4_core/tweets/util/GenerateTweetText_Edinburgh.java
@@ -0,0 +1,120 @@
+/* Generate Edinburgh tweet text in the format of (token1 token2 ...)
+ * Run: sh target/appassembler/bin/GenerateTweetText_Edinburgh -collection {collectionPath} -output {tweetTextPath}
+ */
+package ts4.ts4_core.tweets.util;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.util.List;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.log4j.Logger;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.util.Version;
+import org.mortbay.log.Log;
+
+import cc.twittertools.index.TweetAnalyzer;
+
+import com.google.common.collect.Lists;
+
+public class GenerateTweetText_Edinburgh {
+	private static final Logger LOG = Logger.getLogger(GenerateTweetText_Edinburgh.class);
+	public static final Analyzer ANALYZER = new TweetAnalyzer(Version.LUCENE_43);
+
+	private static final String COLLECTION_OPTION = "collection";
+	private static final String OUTPUT_OPTION = "output";
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) throws Exception {
+		Options options = new Options();
+
+		options.addOption(OptionBuilder.withArgName("path").hasArg()
+				.withDescription("collection location").create(COLLECTION_OPTION));
+		options.addOption(OptionBuilder.withArgName("path").hasArg()
+				.withDescription("output location").create(OUTPUT_OPTION));
+
+		CommandLine cmdline = null;
+		CommandLineParser parser = new GnuParser();
+		try {
+			cmdline = parser.parse(options, args);
+		} catch (ParseException exp) {
+			System.err.println("Error parsing command line: " + exp.getMessage());
+			System.exit(-1);
+		}
+
+		if (!cmdline.hasOption(COLLECTION_OPTION) || !cmdline.hasOption(OUTPUT_OPTION)) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp(GenerateTweetText_Edinburgh.class.getName(), options);
+			System.exit(-1);
+		}
+
+		File collectionLocation = new File(cmdline.getOptionValue(COLLECTION_OPTION));
+		String outputPath = cmdline.getOptionValue(OUTPUT_OPTION);
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputPath, true)));
+		int c = 0;
+		try {
+			FileInputStream fis = new FileInputStream(collectionLocation);
+			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+			String line;
+			while((line = br.readLine()) != null) {
+				String[] arr = line.split("\t");
+				List<String> tokens = parse(ANALYZER, arr[2]);
+				for (String token : tokens) {
+					bw.write(token + " ");
+				}
+				bw.newLine();
+				c ++;
+				if (c % 100000 == 0) {
+					LOG.info(c + " has been processed.");
+				}
+			}
+			try {
+				fis.close();
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		LOG.info("Total " + c + " tweets processed.");
+		bw.close();
+	}
+	
+	public static List<String> parse(Analyzer analyzer, String s) throws IOException {
+		List<String> list = Lists.newArrayList();
+
+		TokenStream tokenStream = analyzer.tokenStream(null, new StringReader(s));
+		CharTermAttribute cattr = tokenStream.addAttribute(CharTermAttribute.class);
+		tokenStream.reset();
+		while (tokenStream.incrementToken()) {
+			if (cattr.toString().length() < 1) {
+				continue;
+			}
+			list.add(cattr.toString());
+		}
+		tokenStream.end();
+		tokenStream.close();
+
+		return list;
+	}
+}
